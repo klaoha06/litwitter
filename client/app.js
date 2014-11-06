f558830c2e79d0c17802f5f7b7be6b21533eee67
Supakorn Laohasongkram <laohasongkram.supakorn@gmail.com>
2014-11-05 19:50:31 -0800

    wed

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
 (function() {
-	var app = angular.module('liTwitter', ['ngRoute', 'ngResource']);
+	var app = angular.module('liTwitter', ['ngRoute', 'ngResource', 'angularMoment']);
 
 	app.factory("getTweets", function($resource) {
 		return $resource("http://localhost:3000/tweets/recent");
@@ -30,6 +30,8 @@
 	var thisTweet;
 
 	app.controller('LTcontroller', function($scope, $http, getTweets, getHashtags) {
+		$scope.now = new moment();
+		// console.log(now.format("HH:mm:ss"));
 		// Handling Tweets
 		if (cachedTweets) {
 			$scope.tweets = cachedTweets;
@@ -41,7 +43,7 @@
 			});
 		}
 
-		// Handling Tweets
+		// Handling Hashtags
 		if (cachedHashtags) {
 			$scope.hashtags = cachedHashtags;
 		}
@@ -54,25 +56,35 @@
 
 		// New Tweet
 		$scope.newTweet = function () {
-			console.log($scope.text);
-			$http.post('http://localhost:3000/tweets', {tweet: {content: $scope.text}}).
+
+			// Differentiating Hashtag and Content
+			var phases = $scope.text.split(' ');
+			var hashtags = [];
+			var regex = /([#]).+/g;
+			for (var i = 0; i < phases.length; i++) {
+				var check_hashtag = (regex.exec(phases[i]));
+				if (check_hashtag !== null) {
+					hashtags.push({name: check_hashtag[0]});
+				}
+			}
+			// Setting Data to Server
+			$http.post('http://localhost:3000/tweets', {hashtags : hashtags, tweet: {content: $scope.text}}).
 			success(function(data){
+				// Updating New Tweet
 				$scope.tweets.unshift(data);
 				$scope.text = '';
 			});
 		};
 	});
 
-	app.controller('tweetCtl', function($scope, $http, $filter, $routeParams, $location, getTweet) {
-		var tweetId = $routeParams.tweetId;
-		if (cachedTweets) {
-			$scope.tweet = $filter('filter')(cachedTweets, {id:tweetId})[0];
-		}
-		else {
-			getTweet.get({id: tweetId}, function(tweet) {
-				$scope.tweet = tweet;
-			});
-		}
-	});
+app.controller('tweetCtl', function($scope, $http, $filter, $routeParams, $location, getTweet) {
+	var tweetId = $routeParams.tweetId;
+	if (cachedTweets) {
+		$scope.tweet = $filter('filter')(cachedTweets, {id:tweetId})[0];
+	}
+	else {
+		$http.get('http://localhost:3000/tweets/search', {hashtags : hashtags, tweet: {content: $scope.text}})
+	}
+});
 
 })();
